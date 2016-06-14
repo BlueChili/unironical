67920ff987c1d616e2737636d0fa27bacd2cc9b8
BlueChili <cmdr.chili@gmail.com>
2016-06-14 13:43:28 -0500

    Initial commit
    repo creation and roots init

---
+++ assets/js/entry.js
@@ -0,0 +1,11 @@
+// const navToggle = document.getElementsByClassName('nv-toggle');
+//
+// navToggle[0].addEventListener('click', function(){
+//   console.log('hello world!');
+// });
+//
+//
+//
+// function theLogger(){
+//   console.log('hi');
+// }
---
+++ assets/js/main.coffee
@@ -0,0 +1,8 @@
+console.log 'hello there!'
+
+
+# navMenu = document.querySelector '.nv-list'
+#
+# navToggle.addEventListener 'click', -> navMenu.classList.toggle 'nv-list_visible'
+#
+# console.log 'heelo world'
